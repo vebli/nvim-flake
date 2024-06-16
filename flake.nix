b01ed00937ca4907aedbf6a7f3988ff199c057d9
vebly <jh.haruna@gmail.com>
2024-06-16 16:27:04 +0200

    .

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "Neovim Configuration";
     inputs = { };
     outputs = {self, nixpkgs, ...}: {
-        default = {config, pkgs, pkgs-unstable, lib, ...}:{
+        homeManagerModule = {config, pkgs, pkgs-unstable, lib, ...}:{
             imports = [ (import ./nvim.nix {inherit config pkgs pkgs-unstable lib;}) ];
         };
     };
