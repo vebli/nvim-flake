1925d9c57c65e422cb332ba292259e0371c77d63
vebly <>
2024-06-29 21:35:55 +0200

    fixed cmp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -27,13 +27,16 @@ cmp.setup({
         { name = 'buffer' },
     })
   })
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
+  -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+  -- Set configuration for specific filetype.
+  --[[ cmp.setup.filetype('gitcommit', {
     sources = cmp.config.sources({
-        { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-        { name = 'buffer' },
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
     })
-})
+ })
+ require("cmp_git").setup() ]]-- 
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
@@ -47,9 +50,11 @@ cmp.setup.cmdline({ '/', '?' }, {
 cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
-        { name = 'path' },
-        { name = 'cmdline' }, 
-    })
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
 })
 
 -- Set up lspconfig.
