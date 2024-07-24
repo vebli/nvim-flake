43493c26cbbd94e7814bfb8f663d0b3d3f3c64e3
vebly <jh.haruna@gmail.com>
2024-07-24 09:22:13 +0200

    now using pkgs.writeShellApplication

--- flake.nix
+++ flake.nix
@@ -1,24 +1,34 @@
 {
-    description = "A very basic flake";
+    description = "Neovim Config";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        nix-systems.url = "github:nix-systems/default";
     };
 
 
-    outputs = { self, nixpkgs }: 
+    outputs = inputs @ { self, nixpkgs, ... }: 
     let 
-        system = "x86_64-linux";
-        overlays = [(import ./nix/overlays.nix)];
-        pkgs = import nixpkgs {inherit system overlays;};
-    in 
-    {
-	inherit overlays;
-        packages.${system}.default = pkgs.neovim;
-        apps.${system}.default = {
-            type = "app";
-            program = "${pkgs.neovim}/bin/nvim";
+        eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
+        mkPkgs = system:
+        import nixpkgs {
+          config = { allowUnfree = true; };
+          inherit system;
+          overlays = [
+            (import ./nix/overlays.nix)
+          ];
         };
-
+    in {
+    packages = eachSystem (system: 
+        let pkgs = mkPkgs system;
+        in {
+            default = pkgs.writeShellApplication {
+                name = "nvim";
+                runtimeInputs = import ./nix/runtime.nix { inherit pkgs; };
+                text = ''
+                    ${pkgs.neovim}/bin/nvim "$@"
+                '';
+            };
+        });
     };
 }
