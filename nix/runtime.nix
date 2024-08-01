c8d871c3fd8842ffbc271acf4b57aa9af04d27ff
vebly <>
2024-08-01 11:21:02 +0200

    added lsps

--- nix/runtime.nix
+++ nix/runtime.nix
@@ -1,3 +1,15 @@
 {pkgs}: with pkgs; [
+
     fzf
+    # LSP
+    cmake-language-server
+    arduino-language-server
+    nodePackages_latest.vls
+    nodePackages_latest.volar
+    tailwindcss-language-server
+    nodePackages_latest.typescript-language-server
+    sqls
+    nil
+    vscode-extensions.rust-lang.rust-analyzer
+    lua-language-server
 ]
